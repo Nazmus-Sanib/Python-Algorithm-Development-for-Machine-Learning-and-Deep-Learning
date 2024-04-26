9e26c3abc727b93d5bf916694752cdf52950d7af
Md. Nazmus Sanib Chowdhury <68843839+Nazmus-Sanib@users.noreply.github.com>
2024-04-27 00:46:15 +0600

    Update convolution_0.py

--- convolution_0.py
+++ convolution_0.py
@@ -1,15 +1,15 @@
-#  Problem count 5: Today let's learn colvolution. 
-# Convolution is used in neural network and computer vision 
+#  Problem count 5: Today let's learn colvolution.
+# Convolution is used in neural network and computer vision
 
 
 import numpy as np
 
-# At first make a matrix which will be convolved over another big matrix 
-# Let's us give this matrix a name, "window"
-# A 3x3 window
-window = np.array([[2,0,2],[0,0,0],[-2,0,-2]])
+# At first make a matrix which will be convolved over another big matrix
+# This matrix is called kernel
+# A 3x3 kernel
+window = np.array([[1,2,1],[0,0,0],[-1,-2,-1]])
 
-# Now let's create another matrix. 
+# Now let's create another matrix.
 # Window would be convolved over this matrix
 matrix = np.random.randint(255, size = 2500).reshape([50,50])[:48,:48] # A 48x48 matrix
 
@@ -22,13 +22,11 @@ def convolution(matrix, window):
   for i in range(int(matrix.shape[0]/window.shape[0]-1)):
     for j in range(int(matrix.shape[1]/window.shape[1]-1)):
       for z in  range(window.shape[0]):
-        conv[s2,s1:s1+3] = np.array([window[z][i2]*matrix[s2][s1+i2] for i2 in range(window.shape[1])])
-        conv[s2+1,s1:s1+3] = np.array([window[z][i2]*matrix[s2+1][s1+i2] for i2 in range(window.shape[1])])
-        conv[s2+2,s1:s1+3] = np.array([window[z][i2]*matrix[s2+2][s1+i2] for i2 in range(window.shape[1])])             
-    
+        conv[s2+z,s1:s1+3] = np.array([window[z][i2]*matrix[s2][s1+i2] for i2 in range(window.shape[1])])
+
         s1 = (j+1)*3
       s2 = (i+1)*3
-    
+
   return conv
 # Now check if we were able to make the convolution
 import matplotlib.pyplot as plt
@@ -38,11 +36,13 @@ plt.contourf(np.hstack([matrix,conv]), cmap = "gray")
 plt.show()
 
 # Now, let's try with a real image
-matrix = plt.imread("drive/MyDrive/download.jpeg")
-matrix = matrix[0:165,0:297,0]
+matrix_ = plt.imread("drive/MyDrive/input.jpeg")
+matrix = matrix_[0:165,0:297,0]
+
 
 import matplotlib.pyplot as plt
 
 conv = convolution(matrix,window)
-plt.contourf(np.hstack([matrix,conv]), cmap = "hsv")
+plt.imshow(np.nanmean(matrix_[0:165,0:297,:], axis = 2), cmap = 'gray')
 plt.show()
+plt.imshow(conv, cmap = "gray")
